dc6958b7cc42862df5929b6f433f480264628c3e
bassasaurus <clearance.clarence@gmail.com>
2021-07-05 22:37:32 -0400

    onFocus modal open

--- app/Screens/FlightCreateScreen.js
+++ app/Screens/FlightCreateScreen.js
@@ -1,32 +1,51 @@
-import React from 'react';
-import { View, StyleSheet, TextInput, Text, Button, Pressable } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, TextInput, Text, Button } from 'react-native';
 import { Formik } from 'formik';
-
-import AppModal from '../components/AppModal';
+import Modal from 'react-native-modal'
+import AppCalendarPicker from '../components/AppCalendarPicker';
  
-function FlightCreateScreen(props) {
+function FlightCreateScreen(props)
+{
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const toggleModal = () => {setModalVisible(!isModalVisible);
+  };
   return (
       <View style={styles.container}>
           <Formik initialValues={{ date: '', route: '' }} >
               {({ values, handleChange }) => (
                   <>
-                      <Pressable>
-                          <AppModal />
-                          <TextInput
-                            value={values.date}
-                            onChangeText={handleChange('date')}
-                            placeholder="Date"
-                            onFocus={ picker => { console.log('onFocus') }} // open modal
-                            onBlur={ picker => {console.log('onBlur')}} // close modal
-                          />
-                      </Pressable>
-                            <TextInput
-                                value={values.route}
-                                onChangeText={handleChange('route')}
-                                placeholder="Route"
-                            />
-                            <Text>{JSON.stringify(values)}</Text>
-                            <Button title='Sign In' />
+                      <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isModalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <AppCalendarPicker/>
+          </View>
+        </View>
+      </Modal>
+    </View>
+                    <TextInput
+                        value={values.date}
+                        onChangeText={handleChange('date')}
+                        placeholder="Date"
+                        onFocus={ () => { toggleModal() }} // open modal
+                        onBlur={ () => {console.log('onBlur')}} // close modal
+                    />
+                    <TextInput
+                        value={values.route}
+                        onChangeText={handleChange('route')}
+                        placeholder="Route"
+                    />
+                    <Text>{JSON.stringify(values)}</Text>
+                    <Button title='Sign In' />
                     </>  
                 )}        
             </Formik>
@@ -35,7 +54,31 @@ function FlightCreateScreen(props) {
                     };
 
 const styles = StyleSheet.create({
-  container: {},
+    container: {},
+
+    modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+    },
+
+    centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+    
 });
 
 export default FlightCreateScreen;
--- app/components/AppCalendarPicker.js
+++ app/components/AppCalendarPicker.js
@@ -3,11 +3,11 @@ import { View, StyleSheet } from 'react-native';
 import CalendarPicker from 'react-native-calendar-picker';
 import STYLES from '../styles/styles'
 
-function AppCalendarPicker(props) {
+function AppCalendarPicker(props){
+
   return (
     <CalendarPicker
       selectedDayColor="lightblue">
-      
     </CalendarPicker>
   );
 };
