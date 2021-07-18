ab3a499abf75cb9f93be9a50d18f1e293e22bfe6
bassasaurus <clearance.clarence@gmail.com>
2021-07-18 16:16:26 -0500

    data fetch onFocus

--- app/components/Picker.js
+++ app/components/Picker.js
@@ -1,5 +1,7 @@
+import { propSatisfies } from "ramda";
 import React, { useState } from "react";
-import { View, StyleSheet, TextInput, Text, Button } from "react-native";
+import { View, StyleSheet, TextInput, Pressable } from "react-native";
+
 import api from "../api/axiosConfig";
 
 function Picker(props) {
@@ -10,16 +12,27 @@ function Picker(props) {
     console.log(result);
     setData(result.data);
   };
+
   return (
     <View style={styles.container}>
-      <Text>Does this even fucking work?</Text>
-      <Button title='fetch' onPress={() => fetchData()}></Button>
+      <TextInput
+        style={styles.input}
+        placeholder={"Aircraft"}
+        onFocus={() => fetchData()}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
 });
 
 export default Picker;
