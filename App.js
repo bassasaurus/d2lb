d0f9319756dc1ade908fe912c2f4d24667f34695
bassasaurus <clearance.clarence@gmail.com>
2021-10-11 13:19:43 -0500

    cleanup before update screen

--- App.js
+++ App.js
@@ -5,16 +5,12 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import StackNavigator from "./app/navigation/StackNavigator";
 import LoginScreen from "./app/screens/LoginScreen";
-import FlightCreateScreen from "./app/screens/FlightCreateScreen";
-import ApproachPicker from "./app/components/ApproachPicker";
 
 export default function App() {
   return (
     <>
       <StatusBar style='auto' />
       {/* <SafeAreaView> */}
-      {/* <ApproachPicker></ApproachPicker> */}
-      {/* <FlightCreateScreen></FlightCreateScreen> */}
 
       {/* <LoginScreen></LoginScreen> */}
       {/* </SafeAreaView> */}
---
+++ app/Screens/FlightCreateScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import FlightForm from "../components/FlightForm";
+
+function FlightCreateScreen(props) {
+  return <FlightForm></FlightForm>;
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default FlightCreateScreen;
