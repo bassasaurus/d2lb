5f5f7502908c4c92be7761fdf7989df4510bd8c4
bassasaurus <clearance.clarence@gmail.com>
2021-10-13 10:14:59 -0500

    removed null values from update data

--- app/Screens/FlightUpdateScreen.js
+++ app/Screens/FlightUpdateScreen.js
@@ -5,6 +5,13 @@ import FlightForm from "../components/FlightForm";
 function FlightUpdateScreen({ route }) {
   const dataToUpdate = route.params.item;
 
+  Object.keys(dataToUpdate).forEach(function (key) {
+    if (dataToUpdate[key] === null) {
+      dataToUpdate[key] = "";
+    }
+    console.log(dataToUpdate);
+  });
+
   return <FlightForm update={true} dataToUpdate={dataToUpdate}></FlightForm>;
 }
 const styles = StyleSheet.create({
