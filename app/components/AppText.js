57bcbcc1c7cfb1cce013f905ceb62cc880391eb1
bassasaurus <clearance.clarence@gmail.com>
2021-11-07 19:47:41 -0500

    add approach styling

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import { STYLES } from "../styles/styles";
 
-function AppText({ children, size, color, weight }) {
+function AppText({ children, size, color, weight, ...props }) {
   return (
     <Text style={{ fontSize: size, color: color, fontWeight: weight }}>
       {children}
