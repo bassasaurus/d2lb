fc37da52b873fff21e55843e63f1f52ac4dea405
bassasaurus <clearance.clarence@gmail.com>
2022-05-11 14:18:38 -0400

    added sentry SDK and AppJS

--- App.js
+++ App.js
@@ -6,6 +6,13 @@ import TailnumberForm from "./app/components/TailnumberForm";
 import { SafeAreaView } from "react-native";
 import TailnumberCreate from "./app/screens/TailnumberCreateScreen";
 import TailnumberCreateScreen from "./app/screens/TailnumberCreateScreen";
+import * as Sentry from "sentry-expo";
+
+Sentry.init({
+  dsn: "YOUR DSN HERE",
+  enableInExpoDevelopment: true,
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
 
 export default function App() {
   const [isSignedIn, setIsSignedIn] = useState(true);
